aa11040673a8368ec83682c5456a2323530a787d
Prima Aulia Gusta <primAulia@gmail.com>
2018-01-23 14:04:02 +0800

    latest code with partials rendering and lt helper with method

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,6 @@
 class ParksController < ApplicationController
+  # skip_before_action :verify_authenticity_token / disable authenticity token
+
   def index
     @all_parks = Park.all.order(created_at: :asc)
   end
@@ -21,5 +23,30 @@ class ParksController < ApplicationController
     @new_park = Park.new
   end
 
-  
+  def create
+    Park.create(park_params)
+    redirect_to parks_path
+  end
+
+  def update
+    @edited_park = Park.find(params[:id])
+    @edited_park.update(park_params)
+    redirect_to @edited_park
+  end
+
+  def destroy
+    park = Park.find(params[:id])
+    park.destroy
+    redirect_to parks_path
+  end
+
+  def edit
+    @searched_park = Park.find(params[:id])
+  end
+
+  private
+
+  def park_params
+    params.require(:park).permit(:name, :description, :picture)
+  end
 end
