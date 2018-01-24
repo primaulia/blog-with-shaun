de0780c7d7090cfd0dd5739346f9e6f58f49582e
Prima Aulia Gusta <primAulia@gmail.com>
2018-01-24 11:02:04 +0800

    push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permit, if: :devise_controller?
+
+  protected
+
+  def configure_permit
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
