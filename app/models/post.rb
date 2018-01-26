48649c3566649be945905750f37c2d83c41dbde9
Prima Aulia Gusta <primAulia@gmail.com>
2018-01-26 12:08:26 +0800

    latest model methods to upvote and downvote

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,38 +12,23 @@ class Post < ApplicationRecord
     # get self post obj
     # build a vote obj with score 1
     # tag it to the current_user
-    vote(1) if can_vote?
+    return vote(1) if can_vote?
+
+    p 'cannot vote anymore'
   end
 
   def downvote
     # same like upvote
     # build a vote obj with score -1
-    vote(-1) if can_vote?
+    return vote(-1) if can_vote?
+
+    p 'cannot vote anymore'
   end
 
   def can_vote?
-    #code
-    user = User.first
-    votes = self.votes
-    votes.each do |x|
-      if x.user_id == user.id
-        return false
-      end
-    end
-  end
-
-  def can_vote?
-    # get current_user
-    # if current_user has vote for self (post obj)
-    # return false
-    # else
-    # return true
-
-    current_user = User.first
-    current_votes = current_user.votes
+    current_votes = current_user.votes #votes from the user
 
     return true if current_votes.empty?
-
     vote_count_by_current_user = current_votes.where(votable_id: self.id, votable_type: 'Post').count
 
     if vote_count_by_current_user == 0
@@ -53,11 +38,19 @@ class Post < ApplicationRecord
     end
   end
 
+  def upvote_count
+    #
+  end
+
+  def downvote_count
+    #
+  end
+
   private
 
   def vote(vote_score)
     new_vote = self.votes.new
-    new_vote.user = User.first
+    new_vote.user = current_user
     new_vote.score = vote_score
     new_vote.save
   end
